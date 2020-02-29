304ea363fbfb7bfc50dd818954a2e48a667b0fe8
Aman Michael <aman.david.michael@gmail.com>
2020-02-29 03:22:44 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
-# Add your variables here
+first_number = 15
+second_number = 9
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+
