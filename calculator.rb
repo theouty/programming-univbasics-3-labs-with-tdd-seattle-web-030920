b141332e05b158688574f01265558ccd6ea0147d
Aman Michael <aman.david.michael@gmail.com>
2020-02-29 03:22:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,9 @@
 first_number = 15
 second_number = 9
 
-sum = first_number - second_number
+sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
+
 
