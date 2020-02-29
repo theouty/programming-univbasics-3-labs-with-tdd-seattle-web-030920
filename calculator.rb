98969b06ed47c8810239ae59b69a24989a66a3a1
Aman Michael <aman.david.michael@gmail.com>
2020-02-29 03:22:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,5 @@ second_number = 9
 sum = first_number - second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient = first_number/second_number
+quotient = first_number / second_number
+
